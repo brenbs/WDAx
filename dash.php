34efecfdc859f49350dd06b49cc4a631f2d9f0f4
brenbs <brendatf188@gmail.com>
2023-05-28 17:40:42 -0300

    Update dash.php

--- dash.php
+++ dash.php
@@ -51,89 +51,78 @@ $result=$conexao->query($sql);
 
 <!--Corpo-->
 <div class="corpo">
-
- <!--Gráfico01-->
-
-  <div id="toptier"><h1>Livros mais alugados</h1>
-
-    <?php
-      //grafico01
-      $sql_mais_alugado = "SELECT nomela FROM alugados  WHERE nomela=nomela GROUP BY nomela ORDER BY COUNT(nomela) DESC LIMIT 1";
-      $resultado_mais_alugado = $conexao->query($sql_mais_alugado);
-      $mais_alugado = $resultado_mais_alugado->fetch_assoc();
-
-      if(isset( $mais_alugado['nomela'])){
-       $mais_alug=  $mais_alugado['nomela'];
-      }
-      //grafico02
-      $sql_grafico01="SELECT nomeua , count(nomela) as user_ativos FROM alugados GROUP BY nomeua ORDER BY count(nomela) DESC limit 5";
-      $resultado_grafico01= $conexao->query($sql_grafico01);
+ <!--Graáfico 01-->
+  <div id="toptier">
+    <?php   
+      $sql_grafico1="SELECT nomela , count(nomela) as quantidade_aluguel FROM alugados WHERE nomela=nomela GROUP BY nomela ORDER BY COUNT(nomela) DESC limit 5";
+      $resultado_grafico1= $conexao->query($sql_grafico1);
       //geração dos dados 
-
-      while($pizza=$resultado_grafico01->fetch_assoc()){
-        $user[]=$pizza['nomeua'];
-        $dados[]=$pizza['user_ativos'];
+      while($pizza=$resultado_grafico1->fetch_assoc()){
+        $nomes[]=$pizza['nomela'];
+        $desc[]=$pizza['quantidade_aluguel'];
       }
     ?>
-    <div id="grafico01">
-      <canvas id="graf" width="400px;" height="400px;"></canvas>
-    </div>
-
+    <h1>Livros mais alugados:</h1>
+    <canvas id='grafico1'></canvas>
   </div>
- <!--Gráfico02-->
- <div class="ativalug">
-  <canvas id="grafico01" style="margin-top:5px;"></canvas>
-  <p style="font-size:1.2em; text-align: center; margin-left: 80px; text-weight:bold;">Livros Mais alugados</p>
 
-  <div class="grafico02">
+  <!--Gráfico 02-->
+  <div id="ativalug">
+   <?php 
+    $sql_graf2="SELECT nomeua , count(nomela) as user_ativos FROM alugados GROUP BY nomeua ORDER BY count(nomela) DESC limit 5";
+    $resultado_graf2= $conexao->query($sql_graf2);
 
-  <canvas id="grafico02"></canvas>
-
-  <p style="font-size:1.2em; text-align: center;">Quantidade de alugueis por Usuário</p>
- </div>
-
-<!--livros devolvidos -->
-
-<div class="lem">
-  <div id="alugA">
-
-  <img src="imagens/livro02.png" alt="imagem1">
-
-    <?php
-    
-    $sql_total_alugueis = "SELECT COUNT(*) AS total_alugueis FROM alugados";
-    
-    $resultado_total_alugueis = $conexao->query($sql_total_alugueis);
-    
-    $linha_total_alugueis = $resultado_total_alugueis->fetch_assoc();
-    
-    if (isset($linha_total_alugueis['total_alugueis'])) {
-    
-      $quantidade_alugueis = $linha_total_alugueis['total_alugueis'];
-    
+    //geração dos dados 
+    while($pizza=$resultado_graf2->fetch_assoc()){
+      $user[]=$pizza['nomeua'];
+      $dados[]=$pizza['user_ativos'];
     }
-    ?>
-    
-    <span class="text"><h3>Total de alugueis Ativos:</h3></span>
-    <span class="value"><?php echo $quantidade_alugueis."<br>"; ?></span>
-    
+   ?>
+   <h1>Aluguéis Ativos</h1>
+   <canvas id='graf2'></canvas>
   </div>
+ <!--livros devolvidos -->
+
+  <div class="lem">
+    <div id="alugA">
+
+    <img src="imagens/livro02.png" alt="imagem1">
+
+      <?php
+      
+      $sql_total_alugueis = "SELECT COUNT(*) AS total_alugueis FROM alugados";
+      
+      $resultado_total_alugueis = $conexao->query($sql_total_alugueis);
+      
+      $linha_total_alugueis = $resultado_total_alugueis->fetch_assoc();
+
+
+      
+      if (isset($linha_total_alugueis['total_alugueis'])) {
+        $quantidade_alugueis = $linha_total_alugueis['total_alugueis'];
+      }
+      ?>
+      
+      <span class="text"><h3>Total de alugueis Ativos:</h3></span>
+      <span class="value"><?php echo $quantidade_alugueis."<br>"; ?></span>
+      
+    </div>
   <div id="livrAtras">
-  <img src="imagens/livr.png" alt="imagem2">
-  <h3>Quantidade de livros atrasados</h3>
-  <?php
-    // total de livros não devolvidos 
+    <img src="imagens/livr.png" alt="imagem2">
+    <h3>Quantidade de livros atrasados</h3>
+    <?php
+      // total de livros não devolvidos 
 
-  $sql_dev="SELECT count(datadev) as fora_prazo FROM alugados where datadev=0";
-  $result_dev = $conexao->query($sql_dev);
-  $total_dev=$result_dev->fetch_assoc();
-  if(isset($total_dev['fora_prazo'])){
-  $total_dev= $total_dev['fora_prazo'];
-  }
+    $sql_dev="SELECT count(datadev) as fora_prazo FROM alugados where datadev=0";
+    $result_dev = $conexao->query($sql_dev);
+    $total_dev=$result_dev->fetch_assoc();
+    if(isset($total_dev['fora_prazo'])){
+    $total_dev= $total_dev['fora_prazo'];
+    }
 
-  echo $total_dev;
+    echo $total_dev;
 
-  ?>
+    ?>
   </div>
 
 
@@ -157,39 +146,38 @@ $result=$conexao->query($sql);
 
   <div id="devolv_prazo">
 
-<img src="imagens/livr.png" alt="">
-<h3>Quantos livros foram devolvidos</h3>
-  
-<?php
+    <img src="imagens/livr.png" alt="">
+    <h3>Quantos livros foram devolvidos</h3>
+      
+    <?php
 
-$sql_livrod="SELECT count(datadev) as dentro_prazo FROM alugados where datadev!=0";
-$result_livrod = $conexao->query($sql_livrod);
-$total_livrod=$result_livrod->fetch_assoc();
-if(isset($total_livrod['dentro_prazo'])){
-$total_livrod= $total_livrod['dentro_prazo'];
-  }
-  echo "Livros devolvidos dentro do prazo: ".$total_livrod."<br>";
-  
-  ?>
-</div>
-
-</div>
+    $sql_livrod="SELECT count(datadev) as dentro_prazo FROM alugados where datadev!=0";
+    $result_livrod = $conexao->query($sql_livrod);
+    $total_livrod=$result_livrod->fetch_assoc();
+    if(isset($total_livrod['dentro_prazo'])){
+    $total_livrod= $total_livrod['dentro_prazo'];
+      }
+      echo "Livros devolvidos dentro do prazo: ".$total_livrod."<br>";
+      
+      ?>
+  </div>
 
 </div>
+</body>
+<!--grafico 01-->
 <script>
-  const ctx = document.getElementById('graf');
- 
+  const ctx = document.getElementById('grafico1');
   new Chart(ctx, {
     type: 'pie',
-
+    
     data: {
-
-      labels: [ "<?php  echo $nomel[0]; ?>","<?php  echo $nomel[1]; ?>","<?php  echo $nomel[2]; ?>","<?php  echo $nomel[3]; ?>","<?php  echo $nomel[4]; ?>"],
+      
+      labels: [ "<?php  echo $nomes[0]; ?>","<?php  echo $nomes[1]; ?>","<?php  echo $nomes[2]; ?>","<?php  echo $nomes[3]; ?>","<?php  echo $nomes[4]; ?>"],
       datasets: [{
-        label: 'Mais Alugados',
+        label: ' Livros Mais Alugados',
         data: ["<?php echo $desc[0]; ?>","<?php echo $desc[1]; ?>","<?php echo $desc[2]; ?>","<?php echo $desc[3]; ?>","<?php echo $desc[4]; ?>"],
         backgroundColor: ['rgba(255,99,132)','rgb(240, 212, 0)','rgb(24, 183, 201)','rgb(62, 201, 100)','rgb(1, 9, 15)'],
-
+        
         borderWidth: 0
       }]
     },
@@ -201,7 +189,30 @@ $total_livrod= $total_livrod['dentro_prazo'];
       }
     }
   });
-
 </script>
-</body>
+<!--grafico 02-->
+
+<script>
+  const graf = document.getElementById('graf2');
+
+  new Chart(graf, {
+    type: 'pie',
+    data: {
+      labels: ["<?php  echo $user[0]; ?>","<?php  echo $user[1]; ?>","<?php  echo $user[2]; ?>","<?php  echo $user[3]; ?>","<?php  echo $user[4]; ?>"],
+      datasets: [{
+        label: 'Alugueis ativos',
+        data: ["<?php echo $dados[0]; ?>","<?php echo $dados[1]; ?>","<?php echo $dados[2]; ?>","<?php echo $dados[3]; ?>","<?php echo $dados[4]; ?>"],
+        borderWidth: 0
+      }]
+    },
+    
+    options: {
+      scales: {
+        y: {
+          beginAtZero: true
+        }
+      }
+    }
+  });
+</script>
 </html>
