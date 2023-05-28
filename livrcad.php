15127419c461a02f8710e72963f505f7644e1560
brenbs <brendatf188@gmail.com>
2023-05-28 17:41:08 -0300

    Update livrcad.php

--- livrcad.php
+++ livrcad.php
@@ -40,28 +40,30 @@ $resulteditora_conect = $conexao -> query($sqleditora_conect);
 <!--formulario-->
 
 <div class="box">
-  <form action="livrcad.php" method="POST">
+  <form action="livrcad.php" method="POST" id="form">
     <fieldset>
      <legend><b>Cadastro de Livros</b></legend>
      <br>
       <div class="inputBox">
       <label for="nomel" class="labelInput">Nome :</label>
-      <input type="text" name="nomel" id="nomel" class="inputUser" required>
+      <input type="text" name="nomel" id="nomel" class="inputUser required"oninput="nameValidate()" >
+      <span class="span-required">*Preencha esse campo corretamente !</span>
       </div>
 
       <br><br>
 
       <div class="inputBox">
         <label for="autor" class="autor">Autor:</label>
-       <input type="autor" name="autor" id="autor" class="inputUser" required>
+       <input type="autor" name="autor" id="autor" class="inputUser required" oninput="autorValidate()" >
+      <span class="span-required">*Preencha esse campo corretamente !</span>
       </div>
 
       <br>
 
       <div class="inputBox"> 
       <label for="editoral" class="labelInput">Editora:</label><br>
-        <select name="editoral" id="" value="Editora:">
-          <option value="Selecionar">Selecionar</option>
+        <select name="editoral" id="selecionar_editora"  class="inputUser required" oninput="editoraValidate()">
+          <option value="0">Selecionar</option>
             <?php
             
               while($editora_data = mysqli_fetch_assoc($resulteditora_conect)){
@@ -71,20 +73,23 @@ $resulteditora_conect = $conexao -> query($sqleditora_conect);
               }
             ?>
         </select>
+      <span class="span-required">*Preencha esse campo corretamente !</span>
       </div>
 
       <br><br>
 
       <div class="inputBox">
        <label for="lanc" class="labelInput">Data de Lançamento:</label>
-       <input type="date" name="lanc" id="lanc" class="inputUser" required>
+       <input type="date" name="lanc" id="lanc" class="inputUser required" oninput="dataValidate()" >
+      <span class="span-required">*Preencha esse campo corretamente !</span>
       </div>
       
       <br><br>
 
       <div class="inputBox">
       <label for="estoque" class="labelInput">Quantidade:</label>
-       <input type="number" name="estoque" id="estoque" class="inputUser" required>
+       <input type="number" name="estoque" id="estoque" class="inputUser required "oninput="estoqueValidate()" >
+      <span class="span-required">*Preencha esse campo corretamente !</span>
       </div>
 
       <br><br>
@@ -94,6 +99,84 @@ $resulteditora_conect = $conexao -> query($sqleditora_conect);
     </fieldset>
   </form>
 </div>
+<script>
+      //pegando dados
+      var form = document.getElementById('form');
+      var campos = document.querySelectorAll('.required');
+      var spans = document.querySelectorAll('.span-required');
+      var data = document.getElementById('lanc');
+      var select = document.getElementById('selecionar_editora');
+      
+      //validação
+      form.addEventListener('submit',(event)=>{
+        if(campos[0].value.length!=0 && campos[1].value.length!=0 && campos[2].value.length!=0 && campos[3].value.length!=0 && campos[4].value.length!=0){
+      
+        }else{
+        nameValidate();
+        autorValidate();
+        editoraValidate();
+        dataValidate();
+        estoqueValidate();
+        event.preventDefault();
+        }
+        
+      })
+        
+      //função alert
+      function setError(index){
+        campos[index].style.color = '#e63636'
+        spans[index].style.display ='block'
+      }
+      function removeError(index){
+        campos[index].style.color = ''
+        spans[index].style.display ='none'
+      }
+      //validar nome
+      function nameValidate(){
+        if(campos[0].value.length < 3){
+           setError(0);
+        }else{
+         removeError(0)
+        }
+      }
+     
+      //autor
+      function autorValidate(){
+        if(campos[1].value.length < 3){
+           setError(1);
+        }else{
+         removeError(1)
+        }
+      }
+       //editora
+      function editoraValidate(){
+        if(select.value==0){
+           setError(2);
+           return;
+        }else{
+         removeError(2);
+        }
+      }
+       //data de lançamento
+       function dataValidate(){
+        if(data.value == ''){
+    		setError(3);
+    		return;
+    	}else{
+        removeError(3);
+      }
+       
+      }
+       //estoque
+      function estoqueValidate(){
+        if(campos[4].value.length< 1){
+           setError(4);
+        }else{
+         removeError(4)
+        }
+      }
+
+    </script>
 </body>
 </html>
 
